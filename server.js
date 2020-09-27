8b64b8cfe111b767c4e4b2b9842ef5aa2bff44dd
Diego Andrade <diegoandradecruz15@gmail.com>
2020-09-26 21:25:18 -0300

    consuming-api

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const axios = require('axios')
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const axios = require("axios");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', async(req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    // response é a resposta do axios MAS eu tiro o data de dentro do response
+    const { data } = await axios("https://api.deezer.com/user/1367329468");
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-    try {
-        // response é a resposta do axios MAS eu tiro o data de dentro do response
-  const { data } =  await axios('https://notes-api-dac.herokuapp.com/note')
-    return res.json(data)
-        
-    } catch (error) {
-        console.error(error)
-    }
-}) 
-
-
-app.listen('4567')
+app.listen(process.env.PORT || 3000);
